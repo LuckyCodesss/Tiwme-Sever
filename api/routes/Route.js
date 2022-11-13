3826d3d6e178f0f782769f731a2f384da492f153
LuckyCodesss <fifagramers@gmail.com>
2022-11-13 22:08:09 +0700

    c

--- api/routes/Route.js
+++ api/routes/Route.js
@@ -3,13 +3,29 @@ const ub = require('../controllers/allController');
 module.exports = app => {
     app
         .route('/users')
-        .get(ub.list_all)
-        .post(ub.create);
+        .get(ub.list_all_user)
+        .post(ub.create_user);
     app
         .route('/users/:userId')
-        .get(ub.read)
-        .put(ub.update)
-        .delete(ub.delete);
+        .get(ub.read_user)
+        .put(ub.update_user)
+        .delete(ub.delete_user);
     app
-        
+        .route('/tools')
+        .get(ub.list_all_tool)
+        .post(ub.create_tool);
+    app
+        .route('/tools/:toolId')
+        .get(ub.read_tool)
+        .put(ub.update_tool)
+        .delete(ub.delete_tool);
+    app
+        .route('/communitys')
+        .get(ub.list_all_community)
+        .post(ub.create_community);
+    app
+        .route('/communitys/:communityId')
+        .get(ub.read_community)
+        .put(ub.update_community)
+        .delete(ub.delete_community);
 }
